3714b8f312328939cd6470dc913d82cac3de85e2
atef <mail@aamri.de>
2021-07-22 20:32:48 +0200

    display quote & author

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 let quote = [];
-const quoteTxt = document.querySelector("#quote");
 const getQuote = document.querySelector("#get-quote");
-
+loadQuote();
 getQuote.addEventListener("click", loadQuote);
 
 function loadQuote() {
-	//data = [];
-
+	//let loadedQuote = undefined;
 	fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
 		.then((response) => response.json())
-		.then((data) => console.log(data));
+		.then(function (data) {
+			quote.push(data);
+			document.getElementById("quote").innerHTML = JSON.stringify(data.data[0].quoteText);
+			document.getElementById("author").innerHTML = JSON.stringify(data.data[0].quoteAuthor);
+		});
 }
